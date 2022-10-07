d6c1527d839658b8cf8d1275dfcc2056cf593231
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-10-07 18:48:05 +0900

    feature/add login page

---
+++ fabricApp/src/main/java/com/inha/fabricApp/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.inha.fabricApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/")
+                .setCacheControl(CacheControl.noCache());
+
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+}
---
+++ fabricApp/src/main/java/com/inha/fabricApp/web/LoginController.java
@@ -0,0 +1,13 @@
+package com.inha.fabricApp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/login")
+public class LoginController {
+
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+}
